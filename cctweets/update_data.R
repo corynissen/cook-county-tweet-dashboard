440ac450cd8bf67809cc5b0379545ea0fb33b388
corynissen <corynissen@gmail.com>
2013-06-26 23:26:42 -0500

    add myURLdecode to handle 'embedded nul in string' error

--- cctweets/update_data.R
+++ cctweets/update_data.R
@@ -5,6 +5,34 @@ library(textcat)
 library(stringr)
 load("c_model.Rdata")
 
+myURLdecode <- function(URL)
+{
+    x <- charToRaw(URL)
+    pc <- charToRaw("%")
+    out <- raw(0L)
+    i <- 1L
+    len <- length(x)
+    while(i <= len) {
+        if(x[i] != pc || i + 2 > len) {
+            out <- c(out, x[i])
+            i <- i + 1L
+        } else {
+            y <- as.integer(x[i + 1L:2L])
+            y[y > 96L] <- y[y > 96L] - 32L # a-f -> A-F
+            y[y > 57L] <- y[y > 57L] - 7L  # A-F
+            if (y[1] > 48L) {
+                y <- sum((y - 48L) * c(16L, 1L))
+                out <- c(out, as.raw(as.character(y)))
+                i <- i + 3L
+            } else {
+                out <- c(out, pc)
+                i <- i + 1L
+            }
+        }
+    }
+    rawToChar(out)
+}
+
 update.df <- function(dataframe){
   created_at2 <- gsub("\\+0000 ", "", dataframe$created_at)
   created_at2 <- parse_date_time(substring(created_at2, 5, nchar(created_at2)), "%b %d %H:%M:%S %Y")
@@ -18,7 +46,7 @@ update.df <- function(dataframe){
 
 do.model <- function(dataframe){
   dataframe$text <- iconv(dataframe$text, "", "latin1", sub="")
-  dataframe$text <- as.character(sapply(dataframe$text, URLdecode))
+  dataframe$text <- as.character(sapply(dataframe$text, myURLdecode))
   dataframe$text.cleansed <- as.character(sapply(dataframe$text, function(x)clean.text(x)))
   dataframe$created_at2 <- as.Date(dataframe$created_at, "%a %b %d %H:%M:%S +0000 %Y")
   dataframe$is.rt <- grepl("^RT| RT @", dataframe$text)
