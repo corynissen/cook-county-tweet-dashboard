2d482ca12d8ffd40c95902b6f8cd192babb7c139
corynissen <corynissen@gmail.com>
2013-06-27 08:10:42 -0500

    change to iconv UTF-8

--- cctweets/update_data.R
+++ cctweets/update_data.R
@@ -45,8 +45,8 @@ update.df <- function(dataframe){
 }
 
 do.model <- function(dataframe){
-  dataframe$text <- iconv(dataframe$text, "", "latin1", sub="")
-  dataframe$text <- as.character(sapply(dataframe$text, myURLdecode))
+  dataframe$text <- iconv(dataframe$text, "", "UTF-8", sub="")
+  #dataframe$text <- as.character(sapply(dataframe$text, myURLdecode))
   dataframe$text.cleansed <- as.character(sapply(dataframe$text, function(x)clean.text(x)))
   dataframe$created_at2 <- as.Date(dataframe$created_at, "%a %b %d %H:%M:%S +0000 %Y")
   dataframe$is.rt <- grepl("^RT| RT @", dataframe$text)
