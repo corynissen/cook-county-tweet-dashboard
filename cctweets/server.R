5a4a093e0e62c7ab7056c152b787ea4c90d6d477
corynissen <corynissen@gmail.com>
2013-06-27 13:53:16 -0500

    reactivate refresh button

--- cctweets/server.R
+++ cctweets/server.R
@@ -8,12 +8,12 @@ load("data.Rdata")
 shinyServer(function(input, output) {
   
   data <- reactive({    
-    #if(input$refresh > 0){
-    #  isolate({
-    #    source("update_data.R")
-    #    load("data.Rdata")
-    #  })
-    #}
+    if(input$refresh > 0){
+      isolate({
+        source("update_data.R")
+        load("data.Rdata")
+      })
+    }
     df$created_at3 <- gsub("\\+0000 ", "", df$created_at)
     df$created_at3 <- parse_date_time(substring(df$created_at3, 5, nchar(df$created_at3)), "%b %d %H:%M:%S %Y")
     df$epoch <- seconds(df$created_at3)
