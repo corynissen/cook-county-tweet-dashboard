731fe3594db98cbc00dd3416452d8c46317288eb
corynissen <corynissen@gmail.com>
2013-09-12 08:04:33 -0500

    updated to reflect my own collection in dynamodb instead of Joe's in mongo

--- getTweets.R
+++ getTweets.R
@@ -1,14 +1,37 @@
 
-library(RMongo)
+library(RJSONIO)
 
-system("sh ~/cn/chicago/keys/mongo_tunnel.sh")
-mongo <- mongoDbConnect("cctweets")
-username = "username here"
-password = "password here"
-authenticated <- dbAuthenticate(mongo, username, password)
-dbShowCollections(mongo)
-df <- dbGetQuery(mongo, "tweets", "{}", 0, 1000000)
-df <- subset(df, X_id!="")
-df$manual_class <- rep(NA, nrow(df))
-write.csv(df, "county_tweets_062413.csv", row.names=F)
+read.tweets <- function(tablename, searchterm, max.id){
+  # throws warning is json is too long
+  if(max.id==""){max.id <- "0"}
+  tweets.json <-suppressWarnings(system(paste0(
+      "python cctweets/get_tweets_since_tweetid_from_dynamo.py '",
+      tablename, "' '",
+      searchterm, "' '", max.id, "'"), intern=T))
+  tweets.json <- paste(tweets.json, collapse="")
+  #tweets.json <- gsub('"', '\\\\"', tweets.json)
+  tweets <- fromJSON(tweets.json)
+  text <- sapply(tweets$tweets, "[[", "text")
+  tweetid <- sapply(tweets$tweets, "[[", "tweetid")
+  author <- sapply(tweets$tweets, "[[", "author")
+  search_term <- sapply(tweets$tweets, "[[", "search_term")
+  timestamp_pretty <- sapply(tweets$tweets, "[[", "timestamp_pretty")
+  df <- as.data.frame(cbind(text=text, author=author, search_term=search_term,
+                            tweetid=tweetid, created.at=timestamp_pretty),
+                      stringsAsFactors=F)
+  return(df)
+}
 
+tablename <- "cory_tweets"
+searchterm <- "cook_county"
+max.id <- "0"
+
+df.new <- read.tweets(tablename, searchterm, max.id)
+df.old <- read.csv("county_tweets.csv", stringsAsFactors=F)
+
+df.new <- subset(df.new, select=c("tweetid", "text"))
+df.new$manual_class <- rep("", nrow(df.new))
+
+df <- rbind(df.old, df.new)
+
+write.csv(df, "county_tweets.csv", row.names=F)
